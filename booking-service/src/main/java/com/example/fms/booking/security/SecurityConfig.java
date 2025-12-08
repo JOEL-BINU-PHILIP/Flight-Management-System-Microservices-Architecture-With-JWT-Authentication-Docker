d51402ed6e0ccc8c5f66c0d433fffecbc46e06e9
JOEL-BINU-PHILIP <joelbinuphilip@gmail.com>
2025-12-08 21:14:22 +0530

    Added security Features to Booking and Flight Service

---
+++ booking-service/src/main/java/com/example/fms/booking/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.fms.booking.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity  // enables @PreAuthorize
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final HeaderAuthFilter headerAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(headerAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
---
+++ flight-service/src/main/java/com/example/fms/flight/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.fms.flight.security;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity     // Enables @PreAuthorize
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final HeaderAuthFilter headerAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()  // All endpoints protected
+                )
+                .addFilterBefore(headerAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
